79cce96302f8f4aaa0aae85925b8e6a67a067d0a
Ben Adams <thundercat@illyriad.co.uk>
2017-08-30 00:48:24 +0100

    Add small Hello World

---
+++ SmallHelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+public class Program
+{
+    public void Configure(IApplicationBuilder app)
+        => app.Run(ctx => ctx.Response.WriteAsync("Hello World!"));
+        
+    static void Main(string[] args)
+        => WebHost.CreateDefaultBuilder(args)
+           .UseStartup<Program>()
+           .Build().Run();
+}
---
+++ SmallHelloWorld/README.md
@@ -0,0 +1,4 @@
+# Small Hello World
+## ASP.NET Core
+
+Is this the smallest single file ASP.NET Core "Hello World!" web app with no extra dependencies?
