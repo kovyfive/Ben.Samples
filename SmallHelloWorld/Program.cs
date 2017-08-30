67dcd549a8d5ad6c367dc9214939f27aa4423952
Nick Craver <nrcraver@gmail.com>
2017-08-29 22:28:59 -0400

    We don't need that namespace!
    Or those extra spaces.

--- SmallHelloWorld/Program.cs
+++ SmallHelloWorld/Program.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
 public class Program
-{       
+{
     static void Main() => WebHost.Start(ctx => ctx.Response.WriteAsync("Hello World!")).WaitForShutdown();
 }
